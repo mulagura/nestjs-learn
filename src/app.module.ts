92bddb0e799c9876268c9a12488635cfc9196ecc
mulagura <mulagura@gmail.com>
2024-03-11 17:19:45 -0300

    modules basics

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, DashboardModule],
   controllers: [AppController],
   providers: [AppService],
 })
